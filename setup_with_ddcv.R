75d5cc9602a40eee6c98affb044b08555443f8f4
Evan Odell <evanodell91@gmail.com>
2020-09-10 13:19:15 +0200

    some minor changes

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -3,7 +3,7 @@ library(here)
 use_datadriven_cv(
   full_name = "Evan Odell",
   data_location = "https://docs.google.com/spreadsheets/d/1VtkzCbTrDlVtoNdpSmAyqZl6R-CnLfilRcbNd_v5k2I/edit?usp=sharing",
-  pdf_location = "https://github.com/evanodell/cv/raw/master/evanodell_cv.pdf",
+  pdf_location = "https://evanodell.com/cv/evanodell_cv.pdf",
   html_location = "evanodell.com/cv/",
   source_location = "https://github.com/evanodell/cv",
   output_dir = here(),
